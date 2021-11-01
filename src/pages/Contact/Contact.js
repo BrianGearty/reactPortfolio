6b9d86bd243db538a956edc53f0df0915ae238e5
BrianGearty <geartybrian@gmail.com>
2021-11-01 17:26:31 -0400

    pushing contact up

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -8,10 +8,13 @@ function sendEmail(e) {
     e.preventDefault();
 
     emailjs.sendForm('gmail', 'template_gsubq8v', e.target, 'user_pqHnzMPwJyjHNYwuZgwiR')
+        .then((target) => {
+            console.log(target)
+            // if (result.text === " ") {
+            //     alert("Please do not send me an empty message.")
+            // }
+        })
         .then((result) => {
-            if(result.text === ""){
-                alert("Please do not send me an empty message.")
-            }
         }, (error) => {
             console.log(error.text);
         });
@@ -22,42 +25,42 @@ function sendEmail(e) {
 const Contact = () => {
     return (
         <div className="contactFade">
-        <div className="container email-container email-bg-pic">
-            <header className="education email-header">
-                <div className="row">
-                    <div className="col-md-12 ">
-                        <h2 className="email-title helllo text-center">CONTACT BRIAN</h2>
-                        <p className="emailText">Interested in working together? Get in touch below.</p>
+            <div className="container email-container email-bg-pic">
+                <header className="education email-header">
+                    <div className="row">
+                        <div className="col-md-12 ">
+                            <h2 className="email-title helllo text-center">CONTACT BRIAN</h2>
+                            <p className="emailText">Interested in working together? Get in touch below.</p>
+                        </div>
                     </div>
-                </div>
-            </header>
+                </header>
 
-            <form onSubmit={sendEmail}>
-                <div className="row pt-5 mx-auto">
-                    <div className="col-8 form-group pt-2 mx-auto">
-                        <input type="text" className="form-control email-window" placeholder="Name" name="name" />
+                <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control email-window" placeholder="Name" name="name" />
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control email-window" placeholder="Email Address" name="email" />
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control email-window" placeholder="Subject" name="subject" />
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control email-window" type="text" id="" cols="30" rows="8" placeholder="Your Message" name="message"></textarea>
+                        </div>
+                        <div className="col-8 form-group mx-auto">
+                            <input type="submit" className="btn btn-email-send" value="Send Message"></input>
+                        </div>
                     </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
-                        <input type="text" className="form-control email-window" placeholder="Email Address" name="email" />
-                    </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
-                        <input type="text" className="form-control email-window" placeholder="Subject" name="subject" />
-                    </div>
-                    <div className="col-8 form-group pt-2 mx-auto">
-                        <textarea className="form-control email-window" type="text" id="" cols="30" rows="8" placeholder="Your Message" name="message"></textarea>
-                    </div>
-                    <div className="col-8 form-group mx-auto">
-                        <input type="submit" className="btn btn-email-send" value="Send Message"></input>
-                    </div>
-                </div>
-            </form>
+                </form>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <Footer />
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <Footer />
-    </div>
 
     );
 };
