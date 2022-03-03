83bc239e939a12b63041adbcb3f63d1ed46fd9eb
briangearty <geartybrian@gmail.com>
2022-03-02 19:35:32 -0500

    stuff

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -21,6 +21,7 @@ function Footer() {
         }
     })
     
+    
         function topFunction() {
             document.body.scrollTop = 0; // For Safari
             document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
